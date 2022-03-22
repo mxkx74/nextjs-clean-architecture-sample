88d68b2859286ae56e5b34843f12cb7099605da5
mxkx74 <0x66ccff@gmail.com>
2022-03-22 19:06:02 +0900

    redux削除

--- src/modules/reducers.ts
+++
@@ -1,9 +0,0 @@
-import { AppState, reducer as app } from './app/index';
-
-export type StoreState = Readonly<{
-  app: AppState;
-}>;
-
-export const reducers = {
-  app,
-};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,10 @@
 import { AppProps } from 'next/app';
-import { Provider } from 'react-redux';
-import { configureStore } from '../store/configureStore';
 import '../styles/globals.css';
 
 const App = ({ Component, pageProps }: AppProps) => (
-  <Provider store={configureStore()}>
+  <main>
     <Component {...pageProps} />
-  </Provider>
+  </main>
 );
 
 export default App;
