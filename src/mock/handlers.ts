15941e5e6edb39128f493bb1a973c5f090b1e987
mxkx74 <0x66ccff@gmail.com>
2022-04-02 19:12:53 +0900

    カスタムquery hook追加

--- src/mock/handlers.ts
+++ src/mock/handlers.ts
@@ -10,7 +10,7 @@ export const handlers = [
   rest.get(path.sample(), (_, res, ctx) => {
     return res(
       ctx.status(200),
-      ctx.delay(5000),
+      ctx.delay(1000),
       ctx.json({
         title: 'CSR Source',
         text: 'Lord of The Rings, is with no absolute hesitation, my most favored and adored book by‑far. The triology is wonderful‑ and I really consider this a legendary fantasy series. It will always keep you at the edge of your seat‑ and the characters you will grow and fall in love with!',
