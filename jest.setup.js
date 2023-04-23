7368190a67af013fb4d30903e14a5e10a1581a73
mxkx74 <0x66ccff@gmail.com>
2023-04-24 00:11:08 +0900

    mockの修正

--- jest.setup.js
+++ jest.setup.js
@@ -1,3 +1,9 @@
 import '@testing-library/jest-dom/extend-expect';
+import { server } from './src/mock';
+
 
 jest.mock('next/router', () => require('next-router-mock'));
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
---
+++ src/core/sampleApi/data/mock.ts
@@ -0,0 +1,63 @@
+import { rest } from 'msw';
+import { path } from '@/constant';
+import { SampleEntity } from '@/core/sampleApi';
+
+export const sampleGetHandler = (status: 200 | 403 | 404 | 500 = 200) => {
+  return rest.get(path.sample(), (_, res, ctx) => {
+    if (status === 403) {
+      return res(ctx.status(403));
+    }
+
+    if (status === 404) {
+      return res(ctx.status(404));
+    }
+
+    if (status === 500) {
+      return res(ctx.status(500));
+    }
+
+    return res(
+      ctx.status(status),
+      ctx.json({
+        id: 1,
+        title: 'page1',
+        text: 'TOP PAGE',
+      } as SampleEntity)
+    );
+  });
+};
+
+export const samplePostHandler = (status: 200 | 403 | 404 | 500 = 200) => {
+  return rest.post(path.sample(), (_, res, ctx) => {
+    if (status === 403) {
+      console.log(403);
+
+      return res(ctx.status(403));
+    }
+
+    if (status === 404) {
+      console.log(404);
+
+      return res(ctx.status(404));
+    }
+
+    if (status === 500) {
+      console.log(500);
+
+      return res(ctx.status(500));
+    }
+    console.log(200);
+
+
+    return res(
+      ctx.status(200),
+      ctx.json({
+        id: 1,
+        title: 'page1',
+        text: 'TOP PAGE',
+      } as SampleEntity)
+    );
+
+  });
+};
+
