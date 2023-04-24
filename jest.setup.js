977dbc6bfd28ed7e1a522cd701c6ce8a14a51fbd
mxkx74 <0x66ccff@gmail.com>
2023-04-24 19:23:13 +0900

    test修正

--- jest.setup.js
+++ jest.setup.js
@@ -1,7 +1,6 @@
 import '@testing-library/jest-dom/extend-expect';
 import { server } from './src/mock';
 
-
 jest.mock('next/router', () => require('next-router-mock'));
 
 beforeAll(() => server.listen());
