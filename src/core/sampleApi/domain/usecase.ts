06e044897dda2ab9f266ffe87baf0c493fcac336
mxkx74 <0x66ccff@gmail.com>
2023-04-24 11:48:36 +0900

    usecaseの追加

---
+++ src/core/sampleApi/domain/usecase.ts
@@ -0,0 +1,60 @@
+import { z } from 'zod';
+import { SampleEntity, sampleSchema } from './entity';
+
+
+/**
+ * repository
+ * @description repositoryのinterface
+ * @param id
+ * @returns Promise<SampleEntity>
+ */
+export type SampleRepository = {
+  get: (id: number) => Promise<SampleEntity>;
+  post: (data: SampleRequestModel) => Promise<SampleEntity>;
+}
+
+/**
+ * request model
+ */
+export const requestModelSchema = z.object({
+  id: z.string(),
+  text: z.string().optional(),
+  title: z.string().optional()
+}).transform((schema) => ({
+  ...schema,
+  id: parseInt(schema.id),
+}));
+
+export type SampleRequestParams = z.input<typeof requestModelSchema>;
+export type SampleRequestModel = z.output<typeof requestModelSchema>;
+
+// 変換用のmapper
+export const sampleRequestMapper = (input: SampleRequestParams): SampleRequestModel => {
+  return requestModelSchema.parse(input);
+};
+
+
+/**
+ * response model
+ * @description entityをviewに渡す前に、必要に応じて変換する
+ */
+export const responseModelSchema = z.object({
+  ...sampleSchema.shape,
+})
+  .transform(
+    (schema) => ({
+      ...schema,
+      id: String(schema.id),
+      text: schema.text ?? '',
+    })
+  );
+
+export type SampleResponseModel = z.output<typeof responseModelSchema>;
+
+// 変換用のmapper
+export const sampleResponseMapper = (entity: SampleEntity): SampleResponseModel => {
+  return responseModelSchema.parse(entity);
+};
+
+
+
