6ed2fb21aec76be137f2794425bc1f863def1cc2
mxkx74 <0x66ccff@gmail.com>
2023-04-24 11:51:40 +0900

    interactorにrepositoryをdi

--- src/core/sampleApi/index.ts
+++ src/core/sampleApi/index.ts
@@ -1,2 +1,9 @@
-export { sampleRepository } from './repository';
-export { useSampleQuery } from './usecase';
+import * as repository from './data/repository';
+import * as interactor from './domain/interactor';
+
+export * from './data/mock';
+export * from './domain/usecase';
+export * from './domain/entity';
+
+// interactorにrepositoryをDIしてexport
+export const sampleInteractor = interactor.sampleInteractor(repository.sampleRepository());
