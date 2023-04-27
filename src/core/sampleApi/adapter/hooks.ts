ab36c8013edb99ec042fdba0137c228582dfa0aa
mxkx74 <0x66ccff@gmail.com>
2023-04-28 02:05:42 +0900

    adapter層でDIする

--- src/core/sampleApi/adapter/hooks.ts
+++ src/core/sampleApi/adapter/hooks.ts
@@ -1,19 +1,22 @@
-import { SampleRequestParams } from '../domain/usecase';
-import { sampleInteractor } from '@/core/sampleApi';
+import { sampleRepository } from '../data/repository';
+import { sampleInteractor } from '../domain/interactor';
+import { type SampleRequestParams } from '../domain/usecase';
 import { useMutationWrapper, useQueryWrapper } from '@/hooks';
 import { queryClient } from '@/lib/queryClient';
+
+const interactor = sampleInteractor(sampleRepository());
 
 export const useGetSample = (id: number) => {
   return useQueryWrapper({
     queryKey: 'sample',
     deps: [id],
-    fetcher: () => sampleInteractor.findById(id)
+    fetcher: () => interactor.findById(id)
   });
 };
 
 export const useCreateSample = () => {
   return useMutationWrapper({
-    fetcher: (data: SampleRequestParams) => sampleInteractor.create(data),
+    fetcher: (data: SampleRequestParams) => interactor.create(data),
     options: {
       onSettled: () => {
         queryClient.invalidateQueries(['sample']);
