e5c0ba78ff1f9690a84b2fd5e7eaa968090540b8
mxkx74 <0x66ccff@gmail.com>
2023-04-29 22:20:40 +0900

    repositoryの調整

--- src/core/sampleApi/data/repository.ts
+++ src/core/sampleApi/data/repository.ts
@@ -2,21 +2,21 @@ import { type SampleEntity, sampleSchema } from '@/core/sampleApi/domain/entity'
 import { type SampleRepository, type SampleRequestModel } from '@/core/sampleApi/domain/usecase';
 import { getFetchClient } from '@/lib/apiClient';
 import { ENDPOINT, path } from '@/constant/path';
-import { deepSnakeToCamel } from '@/helper/object';
+
 
 export const sampleRepository = (): SampleRepository => {
-  const client = getFetchClient(ENDPOINT);
-  const endpoint = path.sample();
+  const client = getFetchClient({ baseURL: ENDPOINT, camelCase: true});
+  const resource = path.sample();
 
   return {
     async findById(id: number) {
-      return client.get<SampleEntity>(endpoint, { params: { id } })
-        .then((response) => sampleSchema.parse(deepSnakeToCamel(response.data)));
+      const response = await client.get<SampleEntity>(resource, { params: { id } });
+      return sampleSchema.parse(response.data);
     },
 
     async create(data: SampleRequestModel) {
-      const { data: response } = await client.post<SampleEntity>(endpoint, data);
-      return sampleSchema.parse(deepSnakeToCamel(response));
+      const response = await client.post<SampleEntity>(resource, data);
+      return sampleSchema.parse(response.data);
     }
   };
 };
