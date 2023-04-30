21265bde5ddce4185df674b9d7d216a342ab8e75
mxkx74 <0x66ccff@gmail.com>
2023-04-30 20:15:02 +0900

    home修正

--- src/components/pages/home/page.tsx
+++ src/components/pages/home/page.tsx
@@ -1,14 +1,15 @@
-import { FC, Suspense } from 'react';
+import { Suspense } from 'react';
+import type { NextPage } from 'next';
 import dynamic from 'next/dynamic';
 import Head from 'next/head';
 
-const Text = dynamic(() => import('../../text/'), { ssr: false });
+const Text = dynamic(() => import('../../text'), { ssr: false });
 
 type Props = {
   title?: string;
 };
 
-export const Component: FC<Props> = ({ title }) => {
+const Home: NextPage = ({ title }: Props) => {
   return (
     <>
       <Head>
@@ -20,3 +21,5 @@ export const Component: FC<Props> = ({ title }) => {
     </>
   );
 };
+
+export default Home;
