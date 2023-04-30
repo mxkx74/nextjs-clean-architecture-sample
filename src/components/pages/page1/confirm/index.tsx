c348705d2b27598768f48677ae5b59fed2815856
mxkx74 <0x66ccff@gmail.com>
2023-04-30 20:09:47 +0900

    confirm修正

--- src/components/pages/page1/confirm/index.tsx
+++
@@ -1,9 +0,0 @@
-import type { NextPageWithLayout } from 'next';
-import Page1 from '@/components/pages/page1';
-import { Component } from './Component';
-
-const Confirm: NextPageWithLayout = () => <Component />;
-
-Confirm.getLayout = Page1.getLayout;
-
-export default Confirm;
---
+++ src/components/pages/page1/confirm/page.tsx
@@ -0,0 +1,22 @@
+import type { NextPageWithLayout } from 'next';
+import Head from 'next/head';
+import Page1 from '@/components/pages/page1/page';
+
+type Props = {
+  title?: string;
+};
+
+const Confirm: NextPageWithLayout = ({ title }: Props) => {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <p>CONFIRM</p>
+    </>
+  );
+};
+
+Confirm.getLayout = Page1.getLayout;
+
+export default Confirm;
