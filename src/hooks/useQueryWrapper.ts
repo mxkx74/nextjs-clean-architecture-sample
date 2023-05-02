1efe2db6920c5417f18aebd3eb0cba52e1a902f3
mxkx74 <0x66ccff@gmail.com>
2023-05-03 00:40:15 +0900

    isErrorBoundaryの切り出し

--- src/hooks/useQueryWrapper.ts
+++ src/hooks/useQueryWrapper.ts
@@ -1,13 +1,13 @@
 import { useQuery } from '@tanstack/react-query';
 import type { ApiQueryKeys } from '@/constant/path';
+import { isInternalError } from '@/helper';
 import type { QueryFunction, QueryKey, UseQueryOptions } from '@tanstack/react-query';
 import type { AxiosError } from 'axios';
-
 
 type Props<
   TQueryFnData = unknown,
   TError = unknown,
-  TData = TQueryFnData, // selectを使用した際のreturn型
+  TData = TQueryFnData // selectを使用した際のreturn型
 > = {
   queryKey?: ApiQueryKeys;
   deps?: QueryKey;
@@ -15,21 +15,9 @@ type Props<
   fetcher: QueryFunction<TQueryFnData | TData, QueryKey>;
 };
 
-export const useQueryWrapper = <T, E, D extends T>({
-  queryKey,
-  deps = [],
-  options,
-  fetcher,
-}: Props<T, E, D>) => {
-  return useQuery(
-    [queryKey, ...deps] as QueryKey,
-    fetcher,
-    {
-      useErrorBoundary: (error) => {
-        const status = error.response?.status;
-        return !!(status && status >= 500);
-      },
-      ...options,
-    }
-  );
+export const useQueryWrapper = <T, E, D extends T>({ queryKey, deps = [], options, fetcher }: Props<T, E, D>) => {
+  return useQuery([queryKey, ...deps] as QueryKey, fetcher, {
+    useErrorBoundary: isInternalError,
+    ...options,
+  });
 };
