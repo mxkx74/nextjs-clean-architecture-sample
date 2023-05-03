e878961463da5c2b5ad56f1a3e8314b5ed76682c
mxkx74 <0x66ccff@gmail.com>
2023-05-03 21:18:40 +0900

    result型をlibからhelperに移動

--- src/helper/network.ts
+++ src/helper/network.ts
@@ -4,3 +4,25 @@ export const isInternalError = <E extends AxiosError<unknown, unknown>>(error: E
   const status = error.response?.status;
   return !!(status && status >= 500);
 };
+
+export type Success<T> = {
+  isSuccess: true;
+  data: T;
+};
+
+export type Failure<T> = {
+  isSuccess: false;
+  data: T;
+};
+
+export const success = <T>(data: T): Success<T> => ({
+  isSuccess: true,
+  data,
+});
+
+export const failure = <T>(data: T): Failure<T> => ({
+  isSuccess: false,
+  data,
+});
+
+export type Result<T, U> = Success<T> | Failure<U>;
