3d73f8263cd1ab187a31d37392bcd0679b9a53e1
mxkx74 <0x66ccff@gmail.com>
2023-05-03 17:01:52 +0900

    useQueryWrapperのtest修正

--- src/__test__/hooks/useQueryWrapper.test.ts
+++ src/__test__/hooks/useQueryWrapper.test.ts
@@ -1,20 +1,47 @@
 import * as Query from '@tanstack/react-query';
-import { renderHook } from '@testing-library/react';
+import { renderHook, waitFor } from '@testing-library/react';
+import { AxiosError } from 'axios';
 import { WithQueryClient } from '@/components/context/WithQueryClient';
-import { ApiQueryKeys } from '@/constant';
 import { useQueryWrapper } from '@/hooks';
 
 describe('useQueryWrapper', () => {
   const wrapper = WithQueryClient();
 
   test('keyとdepsを結合してqueryKeyとして使う', async () => {
-    const Key: ApiQueryKeys = 'sample.index';
-    const deps = [1];
     const options = { useErrorBoundary: false };
     const fetcher = jest.fn();
     const spyUseQuery = jest.spyOn(Query, 'useQuery');
 
-    renderHook(() => useQueryWrapper({ queryKey: Key, deps, options, fetcher }), { wrapper });
-    expect(spyUseQuery).toHaveBeenCalledWith([Key, ...deps], fetcher, options);
+    renderHook(
+      () =>
+        useQueryWrapper({
+          queryKey: 'sample.index',
+          deps: [1],
+          options,
+          fetcher,
+        }),
+      { wrapper }
+    );
+    expect(spyUseQuery).toHaveBeenCalledWith(['sample.index', 1], fetcher, options);
+  });
+
+  describe('失敗時', () => {
+    test('useErrorBoundaryがfalseの場合、エラーをthrowする', async () => {
+      const { result } = renderHook(
+        () => {
+          return useQueryWrapper({
+            queryKey: 'sample.index',
+            fetcher: () => Promise.reject(new AxiosError('error')),
+            deps: [1],
+            options: { useErrorBoundary: false },
+          });
+        },
+        { wrapper }
+      );
+
+      await waitFor(() => {
+        expect(result.current.error?.isAxiosError).toBe(true);
+      });
+    });
   });
 });
