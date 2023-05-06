66745bae5286269b3b0a466e7401935511390904
mxkx74 <0x66ccff@gmail.com>
2023-05-07 00:37:50 +0900

    mock handler追加

--- src/mock/handlers.ts
+++ src/mock/handlers.ts
@@ -1,3 +1,4 @@
 import { sampleGetHandler, samplePostHandler } from '@/feature/sampleApi';
+import { userGetHandler, userPostHandler } from '@/feature/user';
 
-export const handlers = [sampleGetHandler(), samplePostHandler()];
+export const handlers = [sampleGetHandler(), samplePostHandler(), userGetHandler(), userPostHandler()];
