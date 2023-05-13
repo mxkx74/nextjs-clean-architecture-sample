821cc55b05382d6db91fb08d99d23540e15abad3
mxkx74 <0x66ccff@gmail.com>
2023-05-13 16:03:49 +0900

    onSuccessの処理をComponent側に移動

--- src/component/pages/home/page.tsx
+++ src/component/pages/home/page.tsx
@@ -1,18 +1,25 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import styled, { css } from 'styled-components';
 import { type AuthLoginValidation, authLoginValidationSchema, useAuthLogin } from '@/feature/auth';
 import { Button, Label, Stack, TextInput } from '@/component/ui';
+import { route } from '@/constant';
 import { font, mediaQuery, uiColor } from '@/theme';
 
-const Home: NextPage = () => {
-  const { mutate } = useAuthLogin();
+export const Home: NextPage = () => {
+  const { push } = useRouter();
+
+  const { mutate } = useAuthLogin({
+    onSuccess: (data) => push(route.user(data.id)),
+  });
+
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isValid },
   } = useForm<AuthLoginValidation>({
     mode: 'onTouched',
     reValidateMode: 'onChange',
@@ -36,17 +43,19 @@ const Home: NextPage = () => {
           <FormWrapper direction="column" spacing="XXL">
             <FromGroup>
               <Label htmlFor="email">USER NAME</Label>
-              <TextInput id="email" {...register('email')} />
-              {errors.email?.message && <ErrorMessage>{errors.email?.message}</ErrorMessage>}
+              <TextInput id="email" {...register('email')} data-testid="email" />
+              {errors.email?.message && <ErrorMessage role="alert">{errors.email?.message}</ErrorMessage>}
             </FromGroup>
 
             <FromGroup>
               <Label htmlFor="password">PASSWORD</Label>
-              <TextInput id="password" type="password" {...register('password')} />
-              {errors.password?.message && <ErrorMessage>{errors.password?.message}</ErrorMessage>}
+              <TextInput id="password" type="password" {...register('password')} data-testid="password" />
+              {errors.password?.message && <ErrorMessage role="alert">{errors.password?.message}</ErrorMessage>}
             </FromGroup>
 
-            <Button tag="button">LOGIN</Button>
+            <Button tag="button" disabled={!isValid}>
+              LOGIN
+            </Button>
           </FormWrapper>
         </form>
       </Wrapper>
