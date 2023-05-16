7ad7a47af98fe248cb6c2cabfa29ea8236052484
mxkx74 <0x66ccff@gmail.com>
2023-05-17 00:18:39 +0900

    wrapper修正

--- src/hooks/useHookForm.ts
+++ src/hooks/useHookForm.ts
@@ -1,24 +1,11 @@
-import { type FieldValues, type UseFormProps, type SubmitHandler, type UseFormReturn, useForm } from 'react-hook-form';
+import { type FieldValues, type UseFormProps, useForm } from 'react-hook-form';
 
-type Props<T extends FieldValues, Context = unknown> = {
-  onSubmit?: SubmitHandler<T>;
-} & UseFormProps<T, Context>;
-
-export const useHookForm = <T extends FieldValues, Context = unknown>({ onSubmit, ...props }: Props<T, Context>) => {
-  const methods = useForm<T, Context>({
+export const useHookForm = <T extends FieldValues, Context = unknown>(props: UseFormProps<T, Context>) => {
+  return useForm<T, Context>({
     mode: 'onTouched',
     reValidateMode: 'onChange',
     shouldFocusError: true,
     shouldUnregister: false,
     ...props,
   });
-
-  const { handleSubmit } = methods;
-
-  return {
-    ...methods,
-    ...(onSubmit && {
-      handleSubmitWithCallback: handleSubmit(onSubmit),
-    }),
-  } satisfies UseFormReturn<T, Context>;
 };
